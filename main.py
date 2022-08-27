b29ccfdc90aa60cd5b878a8123745d7b17f2d213
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-27 22:58:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
-from fastapi import FastAPI
-from database import create_db_and_tables
+from fastapi import FastAPI,HTTPException
+from database import create_db_and_tables,engine
 from models import Visiter
+from typing import List
+from sqlmodel import Session,select
 
 # Create instance -> FastAPI
 app = FastAPI()
@@ -9,3 +11,12 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+# Create Visiter
+@app.post("/",response_class=List[Visiter],status_code=200)
+async def create_visiter(visiter:Visiter):
+    try:
+        with Session(bind=engine) as session:
+            return session.exec(select(visiter)).all()
+    except:
+        raise HTTPException(status_code=400)
